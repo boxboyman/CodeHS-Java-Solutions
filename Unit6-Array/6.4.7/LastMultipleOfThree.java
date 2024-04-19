9a379f31ef53b6a94b91cedb581217c0538e102e
pail <61991104+boxboyman@users.noreply.github.com>
2024-04-19 12:27:44 -0400

    Update LastMultipleOfThree.java

--- Unit6-Array/6.4.7/LastMultipleOfThree.java
+++ Unit6-Array/6.4.7/LastMultipleOfThree.java
@@ -4,20 +4,20 @@ public class LastMultipleOfThree
     public static void main(String[] args)
     {
         int[] numbers1 = {76, 75, 3, 17, 12, 22, 7};
-        System.out.print("The following index holds the LAST multiple of 3: " + findMultipleOfThree(numbers1));
+        System.out.print("The last multiple of 3 is " + findMultipleOfThree(numbers1));
         
     }
     
     public static int findMultipleOfThree(int[] arr)
     {
         // your code goes here! 
-        for(int i = arr.length - 1; i >= 0; i--)
+        int multiple = -1;
+        for(int i = 0; i<arr.length;i++)
         {
-            if(arr[i] % 3 == 0)
-            {
-                return i;
+            if(arr[i] % 3 ==0){
+                multiple=arr[i];
             }
         }
-        return -1;
+        return multiple;
     }
 }
