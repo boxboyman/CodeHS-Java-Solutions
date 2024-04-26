5c7e24078157da2de65237bc72cc0248dc20a0f8
pail <61991104+boxboyman@users.noreply.github.com>
2024-04-26 11:56:44 -0400

    Update Student.java

--- Unit7-ArrayList/7.2.8/Student.java
+++ Unit7-ArrayList/7.2.8/Student.java
@@ -1,33 +1,20 @@
-import java.util.ArrayList;
 public class Student
 {
+    /*
+     * This class is complete. You do not need
+     * to add anything.
+     */
     private String name;
     private int grade;
-    
-    //Implement classList here:
-    private static ArrayList<Student> classList = new ArrayList<Student>();
     
     public Student(String name, int grade)
     {
         this.name = name;
         this.grade = grade;
-        
-        classList.add(this);
     }
+    
     public String getName()
     {
         return this.name;
-    }
-    /*Don't change the code in this method!
-     This method will print out all the Student names in the classList Array
-     */
-    public static String printClassList()
-    {
-        String names = "";
-        for(Student name : classList)
-        {
-            names += name.getName() + "\n";
-        }
-        return "Student Class List:\n" + names;
     }
 }
