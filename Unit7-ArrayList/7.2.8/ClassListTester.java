8dfafd5f998609daccd0f38cc7b16fb03c8802b2
pail <61991104+boxboyman@users.noreply.github.com>
2024-04-26 11:56:31 -0400

    Update ClassListTester.java

--- Unit7-ArrayList/7.2.8/ClassListTester.java
+++ Unit7-ArrayList/7.2.8/ClassListTester.java
@@ -2,13 +2,11 @@ public class ClassListTester
 {
     public static void main(String[] args)
     {
-        //You don't need to change anything here, but feel free to add more Students!
-        Student alan = new Student("Alan", 11);
-        Student kevin = new Student("Kevin", 10);
-        Student annie = new Student("Annie", 12);
-        System.out.println(Student.printClassList());
+        //You don't need to change anything here
+        Roster.addStudent("Alan", 11);
+        Roster.addStudent("Alex", 10);
+        Roster.addStudent("Anita", 12);
+        System.out.println(Roster.printClassList());
+
     }
-    
-    
-    
 }
