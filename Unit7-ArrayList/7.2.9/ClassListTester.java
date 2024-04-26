985414fdaebeef697f8e1a4d2b725500950a5a15
pail <61991104+boxboyman@users.noreply.github.com>
2024-04-26 11:57:45 -0400

    Update ClassListTester.java

--- Unit7-ArrayList/7.2.9/ClassListTester.java
+++ Unit7-ArrayList/7.2.9/ClassListTester.java
@@ -3,19 +3,17 @@ public class ClassListTester
     public static void main(String[] args)
     {
         //You don't need to change anything here, but feel free to add more Students!
-        Student alan = new Student("Alan", 11);
-        Student kevin = new Student("Kevin", 10);
-        Student annie = new Student("Annie", 12);
-        System.out.println(Student.printClassList());
+        Roster.addStudent("Alan", 11);
+        Roster.addStudent("Alex", 10);
+        Roster.addStudent("Anita", 12);
+        System.out.println(Roster.printClassList());
         
-        System.out.println(Student.getLastStudent());
-        System.out.println(Student.getStudent(1));
+        System.out.println("Last Student: " + Roster.getLastStudent());
+        System.out.println("First Student: " + Roster.getStudent(0));
     
-        Student.addStudent(2, new Student("Trevor", 12));
-        System.out.println(Student.printClassList());
-        System.out.println(Student.getClassSize());
+        System.out.println();
+        Roster.addStudent(2, "Trevor", 12);
+        System.out.println(Roster.printClassList());
+        System.out.println("Class Size: " + Roster.getClassSize());
     }
-    
-    
-    
 }
