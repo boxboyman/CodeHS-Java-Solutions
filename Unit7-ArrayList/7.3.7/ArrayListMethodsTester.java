97468ac2526593d58527252819b6ece2034f3397
pail <61991104+boxboyman@users.noreply.github.com>
2024-04-26 12:01:26 -0400

    Update ArrayListMethodsTester.java

--- Unit7-ArrayList/7.3.7/ArrayListMethodsTester.java
+++ Unit7-ArrayList/7.3.7/ArrayListMethodsTester.java
@@ -4,21 +4,23 @@ public class ArrayListMethodsTester
 {
     public static void main(String[] args)
     {
-        ArrayList<String> stringArray = new ArrayList<String>();
-        stringArray.add("This");
-        stringArray.add("is");
-        stringArray.add("an");
-        stringArray.add("ArrayList");
-        stringArray.add("of");
-        stringArray.add("Strings");
+        ArrayList<Double> prices = new ArrayList<Double>();
         
-        ArrayListMethods.print(stringArray);
+        prices.add(13.45);
+        prices.add(8.99);
+        prices.add(2.95);
+        prices.add(7.88);
+        prices.add(12.56);
+        prices.add(1.23);
+        
+        
+        ArrayListMethods.print(prices);
         System.out.println("\nArrayList is condensing:");
-        ArrayListMethods.condense(stringArray);
-        ArrayListMethods.print(stringArray);
+        ArrayListMethods.condense(prices);
+        ArrayListMethods.print(prices);
         System.out.println("\nArrayList is duplicating:");
-        ArrayListMethods.duplicate(stringArray);
-        ArrayListMethods.print(stringArray);
+        ArrayListMethods.duplicate(prices);
+        ArrayListMethods.print(prices);
         
     }
 }
