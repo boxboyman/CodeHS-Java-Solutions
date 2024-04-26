5e280fd7ea1cf5705cdbbd572554f10351c59b7d
pail <61991104+boxboyman@users.noreply.github.com>
2024-04-25 22:54:07 -0400

    Update RoadTrip.java

--- Unit7-ArrayList/7.3.8/RoadTrip.java
+++ Unit7-ArrayList/7.3.8/RoadTrip.java
@@ -1,42 +1,46 @@
 import java.util.ArrayList;
+
 public class RoadTrip
 {
-    private ArrayList<GeoLocation> places;
+    ArrayList<GeoLocation> trip;
     
     public RoadTrip()
     {
-        this.places = new ArrayList<GeoLocation>();
+        trip = new ArrayList<GeoLocation>();
     }
     // Create a GeoLocation and add it to the road trip
     public void addStop(String name, double latitude, double longitude)
     {
-        places.add(new GeoLocation(name, latitude, longitude));
+        GeoLocation thisStop = new GeoLocation(name, latitude, longitude);
+        trip.add(thisStop);
     }
+    
     // Get the total number of stops in the trip
     public int getNumberOfStops()
     {
-        return places.size();
+        return trip.size();
     }
+    
     // Get the total miles of the trip
     public double getTripLength()
     {
-        double total = 0;
-        for(int i = 1; i < places.size(); i++)
+        double theMiles = 0;
+        for (int i = 0; i < trip.size() - 1; i++)
         {
-            total += places.get(i).distanceFrom(places.get(i - 1));
+            double thisLeg = trip.get(i).distanceFrom(trip.get(i + 1));
+            theMiles += thisLeg;
         }
-        return total;
+        return theMiles;
     }
+    
     // Return a formatted toString of the trip
     public String toString()
     {
-        String list = "";
-        
-        for(int i = 0; i < places.size(); i++)
+        String thisTrip = "";
+        for (int i = 0; i < trip.size(); i++)
         {
-            list += i + 1 + ". " + places.get(i).toString() + "\n";
-            
+            thisTrip += (i + 1) + ". " + trip.get(i) + "\n";
         }
-        return list;
+        return thisTrip;
     }
 }
