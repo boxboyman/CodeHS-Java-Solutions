84fc203713a10bf9424071183cc7cbe7b996cafe
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-02 20:39:33 -0400

    Update Student.java

--- Unit9-Inheritance/9.2.6/Student.java
+++ Unit9-Inheritance/9.2.6/Student.java
@@ -1,24 +1,43 @@
 public class Student
 {
     private String name;
-    private int classYear;
-   
+    private int id;
+    private double gpa;
+    
    // Constructor goes here
-    public Student(String name, int year)
-    {
-        this.name = name;
-        classYear = year;
-    }
+   public Student(String name, int id, double gpa)
+   {
+       this.name = name;
+       this.id = id;
+       this.gpa = gpa;
+   }
+   
    
     public String getName(){
        return name;
     }
+    
+    public void setName(String newName){
+        name = newName;
+    }
    
-    public int getClassYear(){
-       return classYear;
+    public int getID(){
+       return id;
+    }
+    
+    public void setID(int newID){
+        id = newID;
+    }
+    
+    public double getGPA(){
+        return gpa;
+    }
+    
+    public void setGPA(double newGPA){
+        gpa = newGPA;
     }
    
     public String toString(){
-        return name + ", class of " + classYear;
+        return name + " (" + id + ")";
     }
 }
