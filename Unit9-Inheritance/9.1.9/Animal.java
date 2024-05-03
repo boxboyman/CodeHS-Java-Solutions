7df56bfc3deb131f5d78ea971ec4c5a776bd5dbe
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-03 11:56:36 -0400

    Update Animal.java

--- Unit9-Inheritance/9.1.9/Animal.java
+++ Unit9-Inheritance/9.1.9/Animal.java
@@ -1,14 +1,12 @@
 public class Animal
 {
-   private String animal;
-   
-    public void setAnimal(String newAnimal)
-   {
-       animal = newAnimal;
-   }
-   public String getType()
-   {
-       return animal;
-   }
+  private String animal;
+  
+  public void setAnimal(String newAnimal){
+      animal = newAnimal;
+  }
+  public String getType(){
+      return animal;
+  }
    
 }
