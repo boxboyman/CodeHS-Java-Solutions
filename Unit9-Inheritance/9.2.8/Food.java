04849517fc9ea20042a42737e41e5beb79deab71
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-02 20:40:53 -0400

    Update Food.java

--- Unit9-Inheritance/9.2.8/Food.java
+++ Unit9-Inheritance/9.2.8/Food.java
@@ -3,28 +3,22 @@ public class Food
     private String name;
     private int calories;
     
-    public Food (String foodName, int calories)
-    {
-        name = foodName;
-        this.calories = calories;
-    }
-    
-    
-    public String getName()
-    {
-        return name;
-    }
-    public void setName(String name)
+    public Food(String name, int calories)
     {
         this.name = name;
-    }
-    
-    public int getCal()
-    {
-        return calories;
-    }
-    public void setCal(int calories)
-    {
         this.calories = calories;
     }
+    public String getName(){
+        return name;
+    }
+    public int getCalories(){
+        return calories;
+    }
+    
+    public String toString()
+    {
+        return name + " has " + calories + " calories";
+    }
+    
+    
 }
