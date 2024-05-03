fd6abcc52660630ca6879b96febcc5c321e62c0a
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-03 11:54:16 -0400

    Update BookTester.java

--- Unit9-Inheritance/9.1.7/BookTester.java
+++ Unit9-Inheritance/9.1.7/BookTester.java
@@ -2,18 +2,23 @@ public class BookTester
 {
     public static void main(String[] args)
     {
-        Fiction hungerGames = new Fiction();
+        Mystery oneIsLying = new Mystery();
         
-        hungerGames.setPages(374);
-        hungerGames.setName("The Hunger Games");
-        hungerGames.setAuthor("Suzanne Collins");
+        oneIsLying.setPages(374);
+        oneIsLying.setTitle("One of Us Is Lying");
+        oneIsLying.setAuthor("Karen M. McManu");
+        oneIsLying.setSubgenre("Crime Mystery");
         
-        Dict websters = new Dict();
-        websters.setPages(720);
-        websters.setName("Webster's Dictionary");
-        websters.setWords(171476);
         
-        System.out.println(hungerGames.getName());
-        System.out.println(websters.getName());
+        Encyclopedia britannica = new Encyclopedia();
+        britannica.setPages(1009);
+        britannica.setTitle("Original Encyclopedia Britannica, Vol. 3");
+        britannica.setArticles(30);
+        britannica.setStartLetter("C");
+        britannica.setEndLetter("L");
+        
+        
+        System.out.println(oneIsLying.getTitle());
+        System.out.println(britannica.getTitle());
     }
 }
