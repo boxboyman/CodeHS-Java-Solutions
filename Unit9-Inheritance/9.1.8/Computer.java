7cc846521703dbe381743a9273405101c58778af
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-03 11:55:18 -0400

    Update Computer.java

--- Unit9-Inheritance/9.1.8/Computer.java
+++ Unit9-Inheritance/9.1.8/Computer.java
@@ -3,21 +3,17 @@ public class Computer
     private int screenSize;
     private int memory;
     
-    public int getScreenSize()
-    {
+    public int getScreenSize(){
         return screenSize;
     }
-    public void setScreenSize(int newSize)
-    {
+    public void setScreenSize(int newSize){
         screenSize = newSize;
     }
     
-    public int getMemory()
-    {
+    public int getMemory(){
         return memory;
     }
-    public void setMemory(int newMemory)
-    {
+    public void setMemory(int newMemory){
         memory = newMemory;
     }
     
