5bf92eef3de8799e8cd981c2c35c21b267e22b64
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-02 20:42:17 -0400

    Update TShirt.java

--- Unit9-Inheritance/9.2.9/TShirt.java
+++ Unit9-Inheritance/9.2.9/TShirt.java
@@ -1,13 +1,15 @@
 public class TShirt extends Clothing
 {
     private String fabric;
-    public TShirt(String size, String color, String fabric)
-    {
-        super(size, color);
+    
+    public TShirt(String size,String color,String fabric){
+        super(size,color);
         this.fabric = fabric;
     }
-    public String getFabric()
-    {
+    public String getFabric(){
         return fabric;
     }
+    
+    
+    
 }
