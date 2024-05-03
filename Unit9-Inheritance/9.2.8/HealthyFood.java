876f994233475cab0dd330b9fcd41373640a83d6
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-02 20:41:23 -0400

    Update HealthyFood.java

--- Unit9-Inheritance/9.2.8/HealthyFood.java
+++ Unit9-Inheritance/9.2.8/HealthyFood.java
@@ -1,16 +1,17 @@
 public class HealthyFood extends Food
 {
     private String group;
+    private String color;
     
-    public HealthyFood(String foodName, int calories, String foodGroup)
-    {
-        super(foodName, calories);
-        group = foodGroup;
+    public HealthyFood(String name, int calories, String group, String color){
+        super(name,calories);
+        this.group = group;
+        this.color = color;
     }
-    
-    
-    public String getGroup()
-    {
+    public String getGroup(){
         return group;
+    }
+    public String getColor(){
+        return color;
     }
 }
