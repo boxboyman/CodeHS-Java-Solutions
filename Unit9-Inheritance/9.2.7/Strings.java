22cc3f097a9fa830545e15ee04f46bea583ff842
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-02 20:40:26 -0400

    Update Strings.java

--- Unit9-Inheritance/9.2.7/Strings.java
+++ Unit9-Inheritance/9.2.7/Strings.java
@@ -1,19 +1,16 @@
 public class Strings extends Instrument
 {
     private boolean bow;
-    public Strings(String name, boolean bow)
-    {
-        super(name, "Strings");
+  
+    public Strings(String name, boolean bow){
+        super(name,"Strings");
         this.bow = bow;
     }
     
-    public boolean usesBow()
-    {
+    public void setBow(boolean bow){
+        this.bow=bow;
+    }
+    public boolean getBow(){
         return bow;
     }
-    public void setBow(boolean b)
-    {
-        bow = b;
-    }
-       
 }
