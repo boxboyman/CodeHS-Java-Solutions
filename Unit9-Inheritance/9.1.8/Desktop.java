8e9e2212b694efce277969ed04c3d20a1484531c
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-03 11:55:51 -0400

    Update Desktop.java

--- Unit9-Inheritance/9.1.8/Desktop.java
+++ Unit9-Inheritance/9.1.8/Desktop.java
@@ -1,15 +1,13 @@
 public class Desktop extends Computer
 {
     private boolean monitor;
-   
-   public boolean hasMonitor()
-   {
-       return monitor;
-   }
-   public void setMonitor(boolean bool)
-   {
-       monitor = bool;
-   }
-   
+    
+    public boolean hasMonitor(){
+        return monitor;
+    }
+    
+    public void setMonitor(boolean mon){
+        monitor = mon;
+    }
     
 }
