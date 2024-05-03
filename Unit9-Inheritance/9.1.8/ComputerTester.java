9ca49b2770d34a5dd63b664be669adb5f2bf6acb
pail <61991104+boxboyman@users.noreply.github.com>
2024-05-03 11:55:37 -0400

    Update ComputerTester.java

--- Unit9-Inheritance/9.1.8/ComputerTester.java
+++ Unit9-Inheritance/9.1.8/ComputerTester.java
@@ -4,15 +4,19 @@ public class ComputerTester
     {
         Laptop macBook = new Laptop();
         macBook.setBatteryLife(8.5);
+        macBook.setMemory(4);
         
         Desktop dell = new Desktop();
         dell.setScreenSize(18);
         
         Computer surface = new Computer();
         surface.setScreenSize(11);
+        surface.setMemory(8);
         
         System.out.println(macBook.getBatteryLife());
+        System.out.println(macBook.getMemory());
         System.out.println(dell.getScreenSize());
         System.out.println(surface.getScreenSize());
+        System.out.println(surface.getMemory());
     }
 }
